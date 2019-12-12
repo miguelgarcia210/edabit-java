864c0fbbce176e15741f1bcf277a41bed864361f
miguelgarcia210 <garcia.a.miguel.210@gmail.com>
2019-12-12 13:31:40 -0600

    completed challenge

---
+++ src/ex_2_medium/ReturnTheRemainderFromTwoNumbers.java
@@ -0,0 +1,16 @@
+package ex_2_medium;
+
+public class ReturnTheRemainderFromTwoNumbers {
+
+    public static int remainder(int a, int b) {
+        return a % b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(remainder(1, 3));
+        System.out.println(remainder(3, 4));
+        System.out.println(remainder(-9, 45));
+        System.out.println(remainder(5, 5));
+    }
+
+}
